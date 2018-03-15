e0ace36545eb672898c2e4777ecc6fc932ef7cce
Adrian de Lomana <adrianlopezgarciadelomana@gmail.com>
2018-03-14 17:55:32 -0700

    working version

--- extra/proteomics/stoichiometry.2018.03.13.py
+++ extra/proteomics/stoichiometry.2018.03.13.py
@@ -2,8 +2,9 @@
 ### This script computes the stoichiometry of ribosomal proteins over time
 ###
 
-import os,sys,numpy,seaborn,pandas
+import os,sys,numpy
 import matplotlib,matplotlib.pyplot
+import sklearn,sklearn.decomposition,sklearn.manifold
 
 matplotlib.rcParams.update({'font.size':18,'font.family':'Arial','xtick.labelsize':14,'ytick.labelsize':14})
 matplotlib.rcParams['pdf.fonttype']=42
@@ -63,6 +64,7 @@ def dataReader():
                 data[condition][replicate]['tp3vs1'][geneName]=b
                 data[condition][replicate]['tp4vs1'][geneName]=c
 
+
     # sort
     geneNames.sort()
     conditions.sort()
@@ -78,112 +80,51 @@ def figureGrapher():
 
     print('\nanalysis of condition {}...'.format(condition))
     
-    # f.1. initialize variables
-    foldChangeInfo={} # dictionary with all information about fold-change: foldChangeInfo[timePointLabel][riboPtName]=value (fold-change)
-    stoichInfo={} # dictionary with all information about stoichiometry: stoichInfo[timePointLabel][riboPtName]=value (log2 stoichiometry value)
+    # f.1. plotting the first point
+    matplotlib.pyplot.plot([1],[0],'o',color=colors[0],label=timePointLabels[0],mew=0,ms=theDotSize)
 
-    timeStampsViolin=[]; stoichValuesViolin=[]
-    timeStampsSwarm=[]; stoichValuesSwarm=[]
-    timeStampsViolin.append(timePointLabels[0]); stoichValuesViolin.append(0) # incorporate a single point for violin
-    timeStampsSwarm.append(timePointLabels[0]); stoichValuesSwarm.append(0) # incorporate a single point for violin
-
-    significantCases=[]
-    significantPositions=[]
+    # f.2. processing 
     noInfoSet=[]
-
-    # f.2. compute stoichiometries
     for i in range(len(timepoints)):
-
-        timeLabel=timePointLabels[i+1]
-        
-        if timeLabel not in foldChangeInfo:
-            foldChangeInfo[timeLabel]={}
-
-        if timeLabel not in stoichInfo:
-            stoichInfo[timeLabel]={}
-            
+        allFractions=[]
         for ribopt in riboPtNames:
             try:
                 x=numpy.median([data[condition][replicate][timepoints[i]][ribopt] for replicate in replicates])
-                foldChangeInfo[timeLabel][ribopt]=2**x
+                allFractions.append(2**x)
             except:
                 if ribopt not in noInfoSet:
                     noInfoSet.append(ribopt)
                     print('no data for {} {}'.format(timepoints[i],ribopt))
 
-        # f.2.1. compute the stoichiometry per time point
-        localNames=list(foldChangeInfo[timeLabel].keys())
-        allFractions=[foldChangeInfo[timeLabel][localName] for localName in localNames]
+        # f.2. computing the stoichiometry per time point
         theSum=sum(allFractions)
         stoich=(numpy.array(allFractions)/theSum)*len(allFractions)
         log2Stoich=numpy.log2(stoich)
-        for j in range(len(localNames)):
-            stoichInfo[timeLabel][localNames[j]]=log2Stoich[j]
 
-        # f.2.2. finding the limits of 95% of the distribution
+        matplotlib.pyplot.plot(numpy.repeat(i+2,len(stoich)),log2Stoich,'o',color=colors[i+1],alpha=0.1,mew=0,ms=theDotSize,label=timePointLabels[i+1])
+
+        # f.3. finding the limits of 95% of the distribution
         zp=1.959963984540 # taken from https://en.wikipedia.org/wiki/Normal_distribution
         mean=numpy.mean(log2Stoich)
         standardDeviation=numpy.std(log2Stoich)
         low=mean-standardDeviation; high=mean+standardDeviation
-        
-        print('low, high',low,high)
+        print(timepoints[i],mean,standardDeviation,low,high)
+        for element in log2Stoich:
+            if element > high or element < low:
+                matplotlib.pyplot.plot(i+2,element,'o',color=colors[i+1],alpha=1,mew=0,ms=theDotSize)
 
-        # f.2.3. fill up variables for plotting considering significances
-        for i in range(len(localNames)):
-            v=log2Stoich[i]
-            timeStampsViolin.append(timeLabel); stoichValuesViolin.append(v)
-            if v > high or v < low:
-                significantPositions.append([timePointLabels.index(timeLabel),v])
-                if localNames[i] not in significantCases:
-                    significantCases.append(localNames[i])
-            else:
-                timeStampsSwarm.append(timeLabel); stoichValuesSwarm.append(v)
-
-    # f.5. create a dataframe for plotting with seaborn
-    stoichiometryViolin=list(zip(timeStampsViolin,stoichValuesViolin))
-    dfViolin=pandas.DataFrame(data=stoichiometryViolin,columns=['Time points','Stoichiometry'])
-
-    stoichiometrySwarm=list(zip(timeStampsSwarm,stoichValuesSwarm))
-    dfSwarm=pandas.DataFrame(data=stoichiometrySwarm,columns=['Time points','Stoichiometry'])
-    
-    # f.6. plot violin and swarm plots with seaborn
-    colorPalette={}
-    for i in range(len(orderedColors)):
-        colorPalette[timePointLabels[i]]=orderedColors[i]
-
-    ax=seaborn.violinplot(x='Time points',y='Stoichiometry',data=dfViolin,inner=None,linewidth=0,palette=colorPalette)
-    matplotlib.pyplot.setp(ax.collections, alpha=.5)
-
-    ax=seaborn.swarmplot(x='Time points',y='Stoichiometry',data=dfSwarm,color='black',alpha=0.5,size=theDotSize,zorder=2)
-
-    # f.7. plot special points
-    matplotlib.pyplot.plot(0,0,'o',color=orderedColors[0],ms=theDotSize,mew=0,zorder=10)
-
-    # f.8. plot significant cases
-    for case in significantCases:
-        x=[0]
-        y=[0]
-        for i in range(len(timepoints)):
-            timeLabel=timePointLabels[i+1]
-            x.append(i+1)
-            y.append(stoichInfo[timeLabel][case])
-            matplotlib.pyplot.plot(i+1,stoichInfo[timeLabel][case],'o',color='magenta',ms=theDotSize,mew=0)
-        matplotlib.pyplot.plot(x,y,':',color='black',lw=1,alpha=0.3,zorder=0)
-
-    for position in significantPositions:
-        matplotlib.pyplot.plot(position[0],position[1],'sk',ms=theDotSize*2,mew=0)
-
-    # f.7. final figure closing
+    # do consistently the top and bottome 5 percentile
     matplotlib.pyplot.grid(alpha=0.5, ls=':')
     matplotlib.pyplot.xlabel('Time point')
     matplotlib.pyplot.ylabel('Stoichiometry (log$_2$ ribo-pt)')
     matplotlib.pyplot.title(condition)
 
+    matplotlib.pyplot.xticks([1,2,3,4])
     #matplotlib.pyplot.legend(markerscale=1.5,framealpha=1,loc=2,ncol=1,fontsize=14)
 
     figureName='figure.{}.pdf'.format(condition)
     matplotlib.pyplot.tight_layout()
-    #matplotlib.pyplot.axes().set_aspect('equal')
+    matplotlib.pyplot.axes().set_aspect('equal')
     matplotlib.pyplot.savefig(figureName)
     matplotlib.pyplot.clf()    
 
@@ -212,10 +153,9 @@ def riboPtNamesReader():
 dataFolder='/Volumes/omics4tb/alomana/projects/TLR/data/proteomics/all/'
 ribosomalProteinsFile='/Volumes/omics4tb/alomana/projects/TLR/data/ribosomalGeneNames.txt'
 
-orderedColors=['red','orange','green','blue']
+colors=['red','orange','green','blue']
 timePointLabels=['TP1','TP2','TP3','TP4']
-
-theDotSize=3
+theDotSize=8
 
 conditions=['rbf','lysate']
 
