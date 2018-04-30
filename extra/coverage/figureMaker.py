2e3273c4e5e00de6c08baacb2d9468468559093e
Adrian de Lomana <adrianlopezgarciadelomana@gmail.com>
2018-04-30 15:51:04 -0700

    working version of figureMaker

--- extra/coverage/figureMaker.py
+++ extra/coverage/figureMaker.py
@@ -31,25 +31,6 @@ def cdsBlockDefiner(genomicFeature):
 
     return cdsBlocks
 
-def geneAnnotationReader():
-
-    geneAnnotations={}
-
-    with open(gffFile,'r') as f:
-        next(f)
-        next(f)
-        for line in f:
-            vector=line.split('\t')
-            if len(vector) > 3:
-                if vector[2] == 'gene': # check if gene and cds match exact position for all
-                    name=vector[8].split(';')[0].replace('ID=','').replace('_','')
-                    start=int(vector[3])
-                    end=int(vector[4])
-                    strand=vector[6]
-                    geneAnnotations[name]=[start,end,strand]
-
-    return geneAnnotations
-
 def coverageFileReader(dataFileName):
 
     '''
@@ -114,7 +95,7 @@ def dataReader():
 
     # f.1. ribo-pt gene operons
     operonPredictions={}
-    fileName=operonPredictionsDir+'riboPtOperons.edited.txt'
+    fileName=operonPredictionsDir+'riboPtOperons.txt'
     with open(fileName,'r') as f:
         next(f)
         for line in f:
@@ -162,6 +143,7 @@ def figureMaker(genomicFeature,cdsBlocks):
 
         fig=matplotlib.pyplot.figure()
         ax=fig.add_subplot(111)
+        heaven=0
         for timepoint in timepoints:
             y=[]
             for replicate in replicates:
@@ -172,48 +154,51 @@ def figureMaker(genomicFeature,cdsBlocks):
             # compute PDF 
             average=numpy.mean(numpy.array(y),axis=0)
             pdf=average/sum(average)
+            if heaven < numpy.max(pdf):
+                heaven=numpy.max(pdf)
 
             # define the color
             theColor=colors[timepoints.index(timepoint)]
 
             # plot
-            print(pos)
             ax.plot(pos,pdf,'-',color=theColor,label=timepoint)
 
-
-        # f.1.2. incorporate the blocks
+        # f.1.2. make boxes
         strand=cdsBlocks[0][2]
-        print(strand)
-        if strand == '+':
-            ref=cdsBlocks[0][0]
-        else:
-            ref=-cdsBlocks[-1][1]
-
-
-        # f.1.3. making boxes
-        boxSize=(numpy.max(pdf)-numpy.min(pdf))*0.1
-        print(boxSize)    
-
+        boxSize=heaven*0.1
+        postTexty=-boxSize/2
+        
         for block in cdsBlocks:
-            geneName=block[-1]
-            start=block[0]-ref
-            end=block[1]-ref
 
-            geneBox=matplotlib.patches.Rectangle(xy=(start,-boxSize),width=(end-start),height=boxSize,fc='black')
+            geneName=block[-1]
+            boxLabel=synonyms[geneName][0]
+
+            # stand-specific options
+            if strand == '+':
+
+                ref=cdsBlocks[0][0]
+                start=block[0]-ref
+                end=block[1]-ref
+
+            else:
+                
+                ref=cdsBlocks[-1][1]
+                start=ref-block[1]
+                end=ref-block[0]
+
+            # make boxes
+            geneBox=matplotlib.patches.Rectangle(xy=(start,-boxSize),width=(end-start),height=boxSize,fc='white',lw=1,ec='black')
             ax.add_patch(geneBox)
 
+            # define text
             posTextx=start+(end-start)/2
-            postTexty=-boxSize/2
-
-            print(end-start)
             if end-start > 500:
                 theFontSize=10
             else:
                 theFontSize=7
-            ax.text(posTextx,postTexty,geneName,color='white',horizontalalignment='center',verticalalignment='center',fontsize=theFontSize)
-            
+            ax.text(posTextx,postTexty,boxLabel,color='black',horizontalalignment='center',verticalalignment='center',fontsize=theFontSize)
 
-        # f.1.4 final figure closing
+        # f.1.3 final figure closing
         matplotlib.pyplot.xlabel("Relative genomic position (5'->3')")
         matplotlib.pyplot.ylabel('p(coverage)')
         if experiment == 'trna':
@@ -223,20 +208,59 @@ def figureMaker(genomicFeature,cdsBlocks):
 
         matplotlib.pyplot.title('{} {}'.format(genomicFeature,flag))
 
-        matplotlib.pyplot.legend(markerscale=1.5,framealpha=1,loc=1,ncol=2,fontsize=14)
+        matplotlib.pyplot.legend(markerscale=1.5,framealpha=1,loc=0,ncol=2,fontsize=14)
 
-        matplotlib.pyplot.ylim([-1.5*boxSize,boxSize*10+boxSize])
+        matplotlib.pyplot.ylim([-1.5*boxSize,boxSize*10.5])
 
         figureName='figures/figure.{}.{}.pdf'.format(genomicFeature,experiment)
         matplotlib.pyplot.tight_layout()
         matplotlib.pyplot.savefig(figureName)
         matplotlib.pyplot.clf()
 
-    print()
-    if genomicFeature == 'OP0103':
-        sys.exit()
-
     return None
+
+def geneAnnotationReader():
+
+    geneAnnotations={}
+
+    with open(gffFile,'r') as f:
+        next(f)
+        next(f)
+        for line in f:
+            vector=line.split('\t')
+            if len(vector) > 3:
+                if vector[2] == 'gene': # check if gene and cds match exact position for all
+                    name=vector[8].split(';')[0].replace('ID=','')
+                    start=int(vector[3])
+                    end=int(vector[4])
+                    strand=vector[6]
+                    geneAnnotations[name]=[start,end,strand]
+
+    return geneAnnotations
+
+def synonymsReader():
+
+    '''
+    This function reads the GFF3 file and returns a dictionary with synonyms between old and new locus names.
+    '''
+
+    synonyms={}
+    with open(gffFile,'r') as f:
+        for line in f:
+            vector=line.split('\t')
+            if vector[0][0] != '#':
+                info=vector[-1].replace('\n','')
+                if 'old_locus_tag=' in info:
+                    old=info.split('old_locus_tag=')[1].split(';')[0]
+                    new=info.split('ID=')[1].split(';')[0]
+
+                    if '%' in old:
+                        olds=old.split('%2C')
+                        synonyms[new]=[old for old in olds]
+                    else:
+                        synonyms[new]=[old]
+                
+    return synonyms
 
 ###
 ### MAIN
@@ -245,7 +269,7 @@ def figureMaker(genomicFeature,cdsBlocks):
 # 0. user defined variables
 coverageDir='/Volumes/omics4tb/alomana/projects/TLR/data/coverage/'
 operonPredictionsDir='/Volumes/omics4tb/alomana/projects/TLR/data/microbesOnline/'
-gffFile='/Volumes/omics4tb/alomana/projects/TLR/data/genome/hsa.ASM680v1.edited.gff3'
+gffFile='/Volumes/omics4tb/alomana/projects/TLR/data/genome/alo.build.NC002607.NC001869.NC002608.gff3'
 
 timepoints=['tp.1','tp.2','tp.3','tp.4']
 replicates=['rep.1','rep.2','rep.3']
@@ -257,11 +281,17 @@ margin=100 # excess of base pairs
 
 # 1. read data
 print('reading data...')
+
+# 1.1. read gff3 file
 geneAnnotations=geneAnnotationReader()
 
+# 1.2. read operon memberships
 riboOperons,NORPGs=dataReader()
 genomicFeatures=list(riboOperons.keys())+NORPGs
 genomicFeatures.sort()
+
+# 1.3. define synonyms
+synonyms=synonymsReader()
 
 # 2. build figure
 print('building figures...')
