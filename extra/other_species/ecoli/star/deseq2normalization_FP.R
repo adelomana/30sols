d61a296edaac2a70aec3ab6e6e83cd7371ca29a6
Adrian de Lomana <alomana@systemsbiology.org>
2019-11-22 16:20:19 -0800

    added counts for ecoli

--- extra/other_species/ecoli/star/deseq2normalization_FP.R
+++ extra/other_species/ecoli/star/deseq2normalization_FP.R
@@ -2,31 +2,32 @@
 ### This script computes normalized counts for all samples using DESeq2. Normalization is carried out for RNA-seq and Ribo-seq separately.
 ###
 
-# source("https://bioconductor.org/biocLite.R")
-# biocLite("DESeq2")
 library('DESeq2')
 library('stringr')
 
 # 0. user defined variables
-tag='rbf' # trna / rbf
+tag='FP'
 
 setwd("~/scratch/")
 countsDir="/Volumes/omics4tb2/alomana/projects/TLR/data/ecoli/counts"
-DESeqNormalizedCountsFile=paste('//Volumes/omics4tb2/alomana/projects/TLR/data/ecoli//DESeq2/normalizedCounts',tag,'csv',sep='.')
+DESeqNormalizedCountsFile=paste('/Volumes/omics4tb2/alomana/projects/TLR/data/ecoli/deseq2/normalizedCounts',tag,'csv',sep='.')
 
 # 1. handle data reading
 sampleFiles=dir(file.path(countsDir))
-sampleSubset=sampleFiles[str_detect(sampleFiles,tag)]
+sampleSubset=sampleFiles[str_detect(sampleFiles,'SRR')]
+sampleSubset=sampleSubset[1:3]
+print(sampleSubset)
 
 # 1.1. selecting data files and data names
 sampleNames=str_sub(sampleSubset,1,-5)
 
 # 1.2. defining a random design
-timeCondition=sapply(strsplit(sampleNames,split='.',fixed=TRUE),function(x) (paste('tp',x[5],sep='.')))
+theConditions=c('FP','FP','FP')
 
 # 2. formatting variables
-sampleTable=data.frame(sampleName=sampleNames,fileName=sampleSubset,condition=timeCondition)
-dds=DESeqDataSetFromHTSeqCount(sampleTable=sampleTable,directory=countsDir,design= ~ condition)
+sampleTable=data.frame(sampleName=sampleNames,fileName=sampleSubset,condition=theConditions)
+print(sampleTable)
+dds=DESeqDataSetFromHTSeqCount(sampleTable=sampleTable,directory=countsDir,design= ~ 1)
 
 # 3. filtering low abundance transcripts
 keep=rowSums(counts(dds)) >= 10
---
+++ extra/other_species/ecoli/star/deseq2normalization_mRNA.R
@@ -0,0 +1,41 @@
+###
+### This script computes normalized counts for all samples using DESeq2. Normalization is carried out for RNA-seq and Ribo-seq separately.
+###
+
+library('DESeq2')
+library('stringr')
+
+# 0. user defined variables
+tag='mRNA'
+
+setwd("~/scratch/")
+countsDir="/Volumes/omics4tb2/alomana/projects/TLR/data/ecoli/counts"
+DESeqNormalizedCountsFile=paste('/Volumes/omics4tb2/alomana/projects/TLR/data/ecoli/deseq2/normalizedCounts',tag,'csv',sep='.')
+
+# 1. handle data reading
+sampleFiles=dir(file.path(countsDir))
+sampleSubset=sampleFiles[str_detect(sampleFiles,'SRR')]
+sampleSubset=sampleSubset[4:6]
+print(sampleSubset)
+
+# 1.1. selecting data files and data names
+sampleNames=str_sub(sampleSubset,1,-5)
+
+# 1.2. defining a random design
+theConditions=c('mRNA','mRNA','mRNA')
+
+# 2. formatting variables
+sampleTable=data.frame(sampleName=sampleNames,fileName=sampleSubset,condition=theConditions)
+print(sampleTable)
+dds=DESeqDataSetFromHTSeqCount(sampleTable=sampleTable,directory=countsDir,design= ~ 1)
+
+# 3. filtering low abundance transcripts
+keep=rowSums(counts(dds)) >= 10
+dds=dds[keep,]
+
+# 4. normalized values
+vsd=vst(dds,blind=FALSE)
+rld=rlog(dds,blind=FALSE)
+
+# 5. exporting normalized counts
+write.csv(as.data.frame(assay(rld)),file=DESeqNormalizedCountsFile,quote=FALSE)
